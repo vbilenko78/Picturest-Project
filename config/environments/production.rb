bffb705069665bcd5ff2e133c5498f6dda63dce6
vbilenko@78 <vbilenko@yahoo.com>
2015-11-21 20:20:50 +0000

    added devise config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-
+  #Added per Devise
+  config.action_mailer.default_url_options = {host: 'https://famgallery.herokuapp.com'}
   
   config.paperclip_defaults = {
   :storage => :s3,
