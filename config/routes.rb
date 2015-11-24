c399d039f43ee03bd42a27bdc63f237400fe438f
vbilenko@78 <vbilenko@yahoo.com>
2015-11-24 20:51:27 +0000

    made pins main page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about'
 
-  root to: 'home#index'
+  root to: 'pins#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
