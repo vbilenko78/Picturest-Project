7d39b86d26e4e2ae59b36caf5d45d029c28fef67
vbilenko@78 <vbilenko@yahoo.com>
2015-11-24 04:19:01 +0000

    changed backet in models

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,17 @@
- class Pin < ActiveRecord::Base
+#class Pin < ActiveRecord::Base
+ #   belongs_to :user
+  #  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+   # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+# end
+ 
+ 
+class Pin < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
- end
+
+    has_attached_file :image, 
+            :style => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => ENV['udemy-pint']
+    validates :image, presence: true
+    validates :description, presence: true
+end
