472590429554199d2ed7d930992f8c45e9664c3d
vbilenko@78 <vbilenko@yahoo.com>
2015-11-24 03:33:58 +0000

    changed validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
  class Pin < ActiveRecord::Base
     belongs_to :user
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    validates_attachment :image, presence: true,
+    content_type: { content_type: "image/jpeg" }
  end
