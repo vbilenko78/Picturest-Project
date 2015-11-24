f7a9821d6e19f1aaa4a4962b9fce7cbbe6aa24ad
vbilenko@78 <vbilenko@yahoo.com>
2015-11-24 05:10:50 +0000

    updated pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -9,7 +9,7 @@ class Pin < ActiveRecord::Base
     belongs_to :user
 
     has_attached_file :image, 
-            :style => { :medium => "300x300>", :thumb => "100x100>" },
+            :styles => { :medium => "300x300>", :thumb => "100x100>" },
             :storage => :s3,
             :bucket  => ENV['S3_BUCKET_NAME']
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
